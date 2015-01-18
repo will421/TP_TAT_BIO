6798ba81426cfa8f35edf6ef4727fd6c3a5e8d27
will421 <willou.421@gmail.com>
2015-01-18 19:48:50 +0100

    Commentaire decodeur

--- TP-TAT/src/Decodeur.java
+++ TP-TAT/src/Decodeur.java
@@ -15,6 +15,29 @@ public class Decodeur {
 	    return String.format("0x%8s", Integer.toHexString(n)).replace(' ', '0');
 	}
 	
+	/*
+	 * Lis un octet caché dans le buffer donné
+	 * Attention : Prend en compte la position du curseur
+	 */
+	public static short readOne(ShortBuffer buffer)
+	{
+		short res = 0;
+		for(int i =0;i<4;i++) //Pour lire 4 octets dans le buffer
+		{
+			res = (short) (res << 2);
+			
+			if(!buffer.hasRemaining()){ //Si il n'y a plus rien à lire, on rempli le reste de 0
+				res = (byte) (res << 2*(3-i));
+				break;
+			}
+			
+			short b = buffer.get(); //Lecture de l'octet
+			b =(short) (b & 0x3); //Recuperation des deux bits de poids faible
+			res = (short) (res | b); //Ajout au resultat
+		}
+		return res;
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -23,31 +46,23 @@ public class Decodeur {
 		String outfile ="";
 		int beginData = 160;
 		
-		Pixmap tatou = new ShortPixmap(inputFile);
-		short[] bytes = tatou.getShorts();
+		Pixmap tatou = new ShortPixmap(inputFile); //Image tatoué
+		short[] bytes = tatou.getShorts(); 
 		ShortBuffer buffer = ShortBuffer.allocate(bytes.length);
 		buffer.put(bytes);
 		buffer.position(0);
 		
 		//On lit la taille du fichier
 		int sizeRes = 0;
-		System.out.println("taille:"+hex(sizeRes));
-		for(int i =0;i<4;i++)
-		{
+		for(int i =0;i<4;i++){
 			sizeRes = sizeRes <<8;
 			short oct = readOne(buffer);
-			System.out.println(hex(oct));
-			System.out.println("-");
 			sizeRes= sizeRes | oct;
 		}
-		System.out.println("taille:"+sizeRes);
-		System.out.println("taille:"+hex(sizeRes));
-		
-		
+			
 		///On lit le nom du fichier
 		byte[] fileName = new byte[1];
-		for(int i =0;i<16;i++)
-		{
+		for(int i =0;i<16;i++){
 			short oct = readOne(buffer);
 			if(oct ==0)
 				break;
@@ -55,12 +70,12 @@ public class Decodeur {
 			outfile+=new String(fileName);
 		}
 		
+		//Fichier de sorti
 		DataOutputStream out ;
 		try{
 			out = new DataOutputStream(new FileOutputStream(outputPref+outfile));
 		}
-		catch (FileNotFoundException ex)
-		{
+		catch (FileNotFoundException ex){
 			out = new DataOutputStream(new FileOutputStream(outputPref+"File_Error.pgm"));
 		}
 		
@@ -68,37 +83,13 @@ public class Decodeur {
 		//Saut au flux de donnée (-16 car le lecteur de pgm saute l'entête)
 		buffer.position(beginData-16);
 		
-		//On recuperer les deux premier bits de chaque octet
+		//On recuperer les octets caché un par un pour les ecrire dans le fichier de sorti
 		for(int i =0;i<sizeRes;i++)
 		{	
 			short oct = readOne(buffer);
 			out.writeByte((byte)oct);
 		}
 		out.close();
-	}
-
-	public static short readOne(ShortBuffer buffer)
-	{
-		short oct = 0;
-		for(int i =0;i<4;i++)
-		{
-			oct = (short) (oct << 2);
-			
-			if(!buffer.hasRemaining()){
-				oct = (byte) (oct << 2*(3-i));
-				break;
-			}
-			
-			
-			short b = buffer.get();
-			//System.out.println(hex(b));
-			b =(short) (b & 0x3);
-			
-			
-			oct = (short) (oct | b);
-		}
-		
-		return oct;
 	}
 	
 }
